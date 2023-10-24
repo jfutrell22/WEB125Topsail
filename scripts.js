909bc58c6314cf43e7ccda7c68a68b12205b1592
jfutrell22 <117692005+jfutrell22@users.noreply.github.com>
2023-10-24 17:36:46 -0400

    add seed files

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function navFunction() {
+    var x = document.getElementById("links");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
